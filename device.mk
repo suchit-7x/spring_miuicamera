d17d675c55d6fd181245ffd3aedab068cb2bf384
suchit-7x <gtsuchit7x@gmail.com>
2026-08-24 00:28:22 +0530

    spring-miuicam: Add soongnamesapace and rule

--- device.mk
+++ device.mk
@@ -8,9 +8,16 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
 
+# Camera
+$(call soong_config_set_bool,camera,override_format_from_reserved,true)
+
 # Priv-app permission
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
 
 # Sysconfig
 PRODUCT_COPY_FILES += \

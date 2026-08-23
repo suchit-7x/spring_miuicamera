d9a7e8db65135836764278adab275f6ea2bd17ab
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 16:55:43 +0530

    spring-miuicam: Add system properties for MiuiCamera

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,9 @@
 
 MIUICAMERA_PATH := device/xiaomi/spring-miuicam
 
+# Properties
+TARGET_SYSTEM_PROP += $(MIUICAMERA_PATH)/system.prop
+
 # Sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += $(MIUICAMERA_PATH)/sepolicy/vendor
 

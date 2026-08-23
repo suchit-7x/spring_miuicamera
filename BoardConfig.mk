56df3729e8d3dec7b73827a8ec19dfd0387826ca
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 16:04:13 +0530

    spring-miuicam: Initial skeleton tree

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+MIUICAMERA_PATH := device/xiaomi/spring-miuicam
+
+# Inherit from the proprietary version
+include vendor/xiaomi/spring-miuicam/BoardConfigVendor.mk
---
+++ device.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2026 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from the proprietary version
+$(call inherit-product, vendor/xiaomi/spring-miuicam/spring-miuicam-vendor.mk)

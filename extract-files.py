f4fae3ba3bc8552b5c6372138a2590f669fe3231
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 16:57:37 +0530

    spring-miuicam: Add dependency libs

--- extract-files.py
+++ extract-files.py
@@ -24,6 +24,10 @@ blob_fixups: blob_fixups_user_type = {
     'system/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
         .add_needed('libgui_shim_miuicamera.so')
         .sig_replace('08 AD 40 F9', '08 A9 40 F9'),
+    'system/lib64/libcamera_mianode_jni.xiaomi.so': blob_fixup()
+        .add_needed('libgui_shim_miuicamera.so'),
+    'system/lib64/libmicampostproc_client.so': blob_fixup()
+        .remove_needed('libhidltransport.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(

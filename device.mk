b272499966649dd57f246637670fc8ee35866792
suchit-7x <gtsuchit7x@gmail.com>
2026-08-23 16:41:24 +0530

    spring-miuicam: Import permissions and public.libraries.txt from onyx-miuicamera

--- device.mk
+++ device.mk
@@ -4,5 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Public libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
+
+# Priv-app permission
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-miuicamera.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-miuicamera.xml
+
+# Sysconfig
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/miuicamera-hiddenapi-package-allowlist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/miuicamera-hiddenapi-package-allowlist.xml
+
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/spring-miuicam/spring-miuicam-vendor.mk)
